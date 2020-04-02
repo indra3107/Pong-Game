e8fa051d4cb23d77c6538bd148b1bbc158829b17
indra3107 <indra310700@gmail.com>
2020-04-02 14:08:50 +0700

    interval and fire animation

--- Assets/gerakanball.cs
+++ Assets/gerakanball.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class gerakanball : MonoBehaviour
 {
@@ -8,15 +9,36 @@ public class gerakanball : MonoBehaviour
     public int speed;
 
     public Rigidbody2D ball;
+    public Animator FireBall;
 
     void Start()
     {
         ball.velocity = new Vector2(1,-1) * speed;
+        FireBall.SetBool("ismoving", true);
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        if(ball.velocity.x > 0){
+        	ball.GetComponent<Transform>().localScale = new Vector3(1, 1, 1);
+        	}else{
+        		ball.GetComponent<Transform>().localScale = new Vector3(-1, 1, 1);
+        	}
+    }
+
+    void OnCollisionEnter2D(Collision2D other){
+    	if(other.collider.name=="wall kanan" || other.collider.name=="wall kiri"){
+    		StartCoroutine(interval());
+    	}
+    }
+
+    IEnumerator interval(){
+    	ball.velocity = new Vector2(0,0);
+    	FireBall.SetBool("ismoving", false);
+    	ball.GetComponent<Transform>().position = new Vector2(0,0);
+    	yield return new WaitForSeconds(1);
+    	ball.velocity = new Vector2(-1,-1) * speed;
+    	FireBall.SetBool("ismoving", true);
     }
 }
